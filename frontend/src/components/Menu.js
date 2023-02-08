c053b1e187f2ccfe9739ea12da1a555dde3ce63f
vladislav <vladislavchicheg@gmail.com>
2023-02-09 00:30:52 +0400

    homework lesson 2

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Menu = () => {
+
+    return (
+        <ul class="nav nav-pills nav-fill justify-content-center mb-5">
+  <li class="nav-item">
+    <a class="nav-link active" href="/">Пользователи</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Проекты</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ToDo</a>
+  </li>
+  
+</ul>
+    )
+}
+
+export default Menu;
