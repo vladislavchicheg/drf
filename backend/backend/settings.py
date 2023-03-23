3585d77d985432231feed6215a9e97217f6f328c
vladislav <vladislavchicheg@gmail.com>
2023-03-24 00:20:44 +0400

    lesson_12

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'drf_gb',
+        'USER': 'drf',
+        'PASSWORD': 'drf1548!',
     }
 }
 
